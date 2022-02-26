314778c65b54b06d81aa9f2162cefb6565205d53
홍원기pc <dt95@naver.com>
2022-02-27 01:28:00 +0900

    02.26 레이아웃 xml로 화면 구성하기

--- app/src/main/java/org/techtown/homework2/MainActivity.kt
+++ app/src/main/java/org/techtown/homework2/MainActivity.kt
@@ -1,42 +1,13 @@
 package org.techtown.homework2
 
-import android.graphics.Typeface
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val name = TextView(this).apply {
-            typeface = Typeface.DEFAULT_BOLD
-            text = "budapest"
-        }
-
-        val image = ImageView(this).also {
-            it.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.budapest))
-        }
-
-        val address = TextView(this).apply {
-            typeface = Typeface.DEFAULT_BOLD
-            text = "budapest, Hungary"
-        }
-        val layout = LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
-            addView(name, WRAP_CONTENT, WRAP_CONTENT)
-            addView(image, WRAP_CONTENT, WRAP_CONTENT)
-            addView(address, WRAP_CONTENT, WRAP_CONTENT)
-        }
-
-        setContentView(layout)
-
-        }
     }
+
+}
